3d91e932d9d2cca8b05f5ab9ba6734f49736e013
unit-a-user <helixd.2s18@gmail.com>
2022-11-30 03:17:12 +0800

    remove redundant

--- test.js
+++ test.js
@@ -21,63 +21,6 @@ Object.defineProperty(Array.prototype, 'chunk', {value: function(n) {
     return new Array(Math.ceil(this.length/n)).fill().map((_,i) => this.slice(i*n,i*n+n));
 }});
 
-
-function inverse(_A) {
-    var temp,
-    N = _A.length,
-    E = [];
-   
-    for (var i = 0; i < N; i++)
-      E[i] = [];
-   
-    for (i = 0; i < N; i++)
-      for (var j = 0; j < N; j++) {
-        E[i][j] = 0;
-        if (i == j)
-          E[i][j] = 1;
-      }
-   
-    for (var k = 0; k < N; k++) {
-      temp = _A[k][k];
-   
-      for (var j = 0; j < N; j++)
-      {
-        _A[k][j] /= temp;
-        E[k][j] /= temp;
-      }
-   
-      for (var i = k + 1; i < N; i++)
-      {
-        temp = _A[i][k];
-   
-        for (var j = 0; j < N; j++)
-        {
-          _A[i][j] -= _A[k][j] * temp;
-          E[i][j] -= E[k][j] * temp;
-        }
-      }
-    }
-   
-    for (var k = N - 1; k > 0; k--)
-    {
-      for (var i = k - 1; i >= 0; i--)
-      {
-        temp = _A[i][k];
-   
-        for (var j = 0; j < N; j++)
-        {
-          _A[i][j] -= _A[k][j] * temp;
-          E[i][j] -= E[k][j] * temp;
-        }
-      }
-    }
-   
-    for (var i = 0; i < N; i++)
-      for (var j = 0; j < N; j++)
-        _A[i][j] = E[i][j];
-    return _A;
-  }
-
 //
 (async()=>{
     const instanceObj = new K.InstanceObj({  });
